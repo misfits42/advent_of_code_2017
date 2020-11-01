6fc931e5b850b73e96af305de26c5cd3ccf9a64f
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-11-01 17:31:16 +1100

    Updated implementation of Day 07 generator function

--- src/day_07.rs
+++ src/day_07.rs
@@ -1,80 +1,52 @@
 use regex::Regex;
 
+use std::collections::HashMap;
 use std::collections::HashSet;
 
-struct ProgramNode {
-    name: String,
-    weight: u64,
-    children: Vec<String>
-}
-
-impl ProgramNode {
-    pub fn new(name: String, weight: u64, children: Vec<String>) -> Self {
-        Self {
-            name: name,
-            weight: weight,
-            children: children.to_vec()
-        }
-    }
-
-    pub fn get_name(&self) -> String {
-        return self.name.to_owned();
-    }
-
-    pub fn get_weight(&self) -> u64 {
-        return self.weight;
-    }
-
-    pub fn get_children(&self) -> Vec<String> {
-        return self.children.to_vec();
-    }
-
-    pub fn get_num_children(&self) -> usize {
-        return self.children.len();
-    }
-}
-
 #[aoc_generator(day7)]
-fn generate_input(input: &str) -> Vec<ProgramNode> {
-    let mut nodes = Vec::<ProgramNode>::new();
+fn generate_input(input: &str) -> (HashMap::<String, Vec<String>>, HashMap<String, u64>) {
+    let mut rel_map = HashMap::<String, Vec<String>>::new();
+    let mut weight_map = HashMap::<String, u64>::new();
+
     let no_child_regex = Regex::new(r"([a-z]+) \((\d+)\)").unwrap();
     let with_child_regex = Regex::new(r"([a-z]+) \((\d+)\) -> (.*)").unwrap();
     for line in input.lines() {
         let line = line.trim();
         if with_child_regex.is_match(line) {
             for capture in with_child_regex.captures_iter(line) {
-                let name = capture[1].to_owned();
+                let name = capture[1].to_string();
                 let weight = capture[2].parse::<u64>().unwrap();
                 let children = capture[3].split(", ").map(|x| String::from(x)).collect::<Vec<String>>();
-                // println!("{:?}", children);
-                nodes.push(ProgramNode::new(name, weight, children));
+                rel_map.insert(name.to_string(), children);
+                weight_map.insert(name.to_string(), weight);
                 break; // Regex should only match once to the line
             }
         } else {
             for capture in no_child_regex.captures_iter(line) {
-                let name = capture[1].to_owned();
+                let name = capture[1].to_string();
                 let weight = capture[2].parse::<u64>().unwrap();
                 let children = Vec::<String>::new();
-                nodes.push(ProgramNode::new(name, weight, children));
+                rel_map.insert(name.to_string(), children);
+                weight_map.insert(name.to_string(), weight);
                 break; // Regex should only match once to the line
             }
         }
     }
-    return nodes;
+    return (rel_map, weight_map);
 }
 
 #[aoc(day7, part1)]
-fn solve_part_1(input: &Vec<ProgramNode>) -> String {
+fn solve_part_1(input: &(HashMap::<String, Vec<String>>, HashMap<String, u64>)) -> String {
     let mut all_children = HashSet::<String>::new();
-    let mut all_names = Vec::<String>::new();
-    // Find out all names and all nodes that are a child to another node
-    for node in input {
-        all_names.push(node.get_name());
-        for child in node.get_children() {
-            all_children.insert(child);
+    let mut all_names = HashSet::<String>::new();
+    // Extract all node names and all nodes that are child to another node
+    for (parent, children) in &input.0 {
+        all_names.insert(parent.to_string());
+        for child in children {
+            all_children.insert(child.to_string());
         }
     }
-    // Look for the bottom node - the one that is not a child to another node
+    // Determine the bottom node (not a child to any other node)
     for name in all_names {
         if !all_children.contains(&name) {
             return name;
@@ -84,6 +56,6 @@ fn solve_part_1(input: &Vec<ProgramNode>) -> String {
 }
 
 #[aoc(day7, part2)]
-fn solve_part_2(input: &Vec<ProgramNode>) -> String {
+fn solve_part_2(input: &(HashMap::<String, Vec<String>>, HashMap<String, u64>)) -> String {
     unimplemented!();
 }
