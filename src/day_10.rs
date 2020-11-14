72db7ad47a593da802970c5dcb61a236cbfab6bf
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-11-14 20:35:06 +1100

    Solved Day 10, Part 2

--- src/day_10.rs
+++ src/day_10.rs
@@ -42,7 +42,7 @@ fn solve_part_1(input: &String) -> u64 {
     // Initialise the list, current position and skip size
     let mut list = (0..=255).collect::<Vec<u8>>();
     let mut current_pos: usize = 0;
-    let mut skip_size = 0;
+    let mut skip_size: usize = 0;
     // Process each length
     perform_knot_hash_round(&mut list, &lengths, &mut current_pos, &mut skip_size);
     // Return the product of the first two values in the list after processing
@@ -50,8 +50,32 @@ fn solve_part_1(input: &String) -> u64 {
 }
 
 #[aoc(day10, part2)]
-fn solve_part_2(input: &String) -> u64 {
-    unimplemented!();
+fn solve_part_2(input: &String) -> String {
+    // Determine lengths from input string - and append set lengths after processing input
+    let mut lengths = input.chars().map(|x| x as usize).collect::<Vec<usize>>();
+    lengths.append(&mut vec![17, 31, 73, 47, 23]);
+    // Initialise the list, current position and skip size
+    let mut list = (0..=255).collect::<Vec<u8>>();
+    let mut current_pos: usize = 0;
+    let mut skip_size: usize = 0;
+    // Calculate parse hash by performing 64 rounds of knot hash alg
+    for _ in 0..64 {
+        perform_knot_hash_round(&mut list, &lengths, &mut current_pos, &mut skip_size);
+    }
+    // Calculate dense hash
+    let mut dense_hash: Vec<u8> = vec![0; 16];
+    for group in 0..16 {
+        let start = group * 16;
+        // Calculate the result of XOR'ing the 16 values in the current group
+        let mut xor_res: u8 = 0;
+        for i in 0..16 {
+            xor_res ^= list[start + i];
+        }
+        dense_hash[group] = xor_res;
+    }
+    // Convert dense hash to hexadecimal representation
+    let hex_output = hex::encode(dense_hash);
+    return hex_output;
 }
 
 #[cfg(test)]
@@ -63,5 +87,12 @@ mod tests {
         let input = generate_input(&std::fs::read_to_string("./input/2017/day10.txt").unwrap());
         let result = solve_part_1(&input);
         assert_eq!(38628, result);
+    }
+
+    #[test]
+    fn test_d10_p2_proper() {
+        let input = generate_input(&std::fs::read_to_string("./input/2017/day10.txt").unwrap());
+        let result = solve_part_2(&input);
+        assert_eq!("e1462100a34221a7f0906da15c1c979a", result);
     }
 }
