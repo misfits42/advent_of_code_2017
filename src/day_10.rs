cd6ce016fa411dad7dec39d12046fa6a9baa1953
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-11-14 19:49:16 +1100

    Refactored knot hash round into function

--- src/day_10.rs
+++ src/day_10.rs
@@ -3,20 +3,18 @@ fn generate_input(input: &str) -> String {
     return String::from(input.trim());
 }
 
-#[aoc(day10, part1)]
-fn solve_part_1(input: &String) -> u64 {
-    // Convert the input into list of lengths
-    let lengths = input.split(",").map(|x| x.parse::<usize>().unwrap());
-    // Initialise the list, current position and skip size
-    let mut list = (0..=255).collect::<Vec<u8>>();
-    let mut current_pos: usize = 0;
-    let mut skip_size = 0;
+fn perform_knot_hash_round(
+    list: &mut Vec<u8>,
+    lengths: &Vec<usize>,
+    current_pos: &mut usize,
+    skip_size: &mut usize,
+) {
     // Process each length
     for length in lengths {
         // Calculate list of indices included in the reverse
         let mut rev_indices = Vec::<usize>::new();
-        for i in 0..length {
-            let index = (current_pos + i) % list.len();
+        for i in 0..*length {
+            let index = (*current_pos + i) % list.len();
             &rev_indices.push(index);
         }
         // Extract values from the list then reverse
@@ -31,10 +29,22 @@ fn solve_part_1(input: &String) -> u64 {
             list[list_index] = extract_list[i]
         }
         // Move the current positon
-        current_pos = (current_pos + length + skip_size) % list.len();
+        *current_pos = (*current_pos + length + *skip_size) % list.len();
         // Increase skip size by 1
-        skip_size += 1;
+        *skip_size += 1;
     }
+}
+
+#[aoc(day10, part1)]
+fn solve_part_1(input: &String) -> u64 {
+    // Convert the input into list of lengths
+    let lengths = input.split(",").map(|x| x.parse::<usize>().unwrap()).collect::<Vec<usize>>();
+    // Initialise the list, current position and skip size
+    let mut list = (0..=255).collect::<Vec<u8>>();
+    let mut current_pos: usize = 0;
+    let mut skip_size = 0;
+    // Process each length
+    perform_knot_hash_round(&mut list, &lengths, &mut current_pos, &mut skip_size);
     // Return the product of the first two values in the list after processing
     return list[0] as u64 * list[1] as u64;
 }
@@ -42,4 +52,16 @@ fn solve_part_1(input: &String) -> u64 {
 #[aoc(day10, part2)]
 fn solve_part_2(input: &String) -> u64 {
     unimplemented!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_d10_p1_proper() {
+        let input = generate_input(&std::fs::read_to_string("./input/2017/day10.txt").unwrap());
+        let result = solve_part_1(&input);
+        assert_eq!(38628, result);
+    }
 }
